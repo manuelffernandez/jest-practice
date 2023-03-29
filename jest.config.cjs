83f8306a8662aa4d686444d3fd721b28367f0811
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-03-29 16:59:36 -0300

    add coverage config

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
   transform: {
     '\\.[jt]sx?$': 'babel-jest',
   },
